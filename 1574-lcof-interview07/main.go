797ae50ff47148775ea877912c894cc49e88fb4b
yuanhao <396281278@qq.com>
2020-05-13 21:37:03 +0800

    2020-05-12 leetcode打卡

---
+++ 1574-lcof-interview07/main.go
@@ -0,0 +1,27 @@
+package _574_lcof_interview07
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	splits := make(map[int]int, len(inorder))
+	for i := range inorder {
+		splits[inorder[i]] = i
+	}
+	return helper(preorder, 0, splits, 0, len(inorder)-1)
+}
+
+func helper(preOrder []int, index int, splits map[int]int, begin, end int) *TreeNode {
+	if begin > end {
+		return nil
+	}
+	val := preOrder[index]
+	split := splits[val]
+	root := &TreeNode{Val: val}
+	root.Left = helper(preOrder, index+1, splits, begin, split-1)
+	root.Right = helper(preOrder, index+(split-begin+1), splits, split+1, end)
+	return root
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
