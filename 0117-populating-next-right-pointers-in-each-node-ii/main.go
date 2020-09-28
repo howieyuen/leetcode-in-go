4ef058347283cb5b2a1c07f6e1c687ab066397bb
Hao Yuan <haoyuan@mail.ustc.edu.cn>
2020-09-29 00:02:51 +0800

    2020-09-28 leetcode打卡

--- 0117-populating-next-right-pointers-in-each-node-ii/main.go
+++ 0117-populating-next-right-pointers-in-each-node-ii/main.go
@@ -1,12 +1,12 @@
 package _117_populating_next_right_pointers_in_each_node_ii
 
-func connect(root *TreeNode) *TreeNode {
+func connect(root *Node) *Node {
 	if root == nil {
 		return nil
 	}
 	cur := root
 	for cur != nil {
-		dummy := &TreeNode{}
+		dummy := &Node{}
 		head := dummy
 		for cur != nil {
 			if cur.Left != nil {
@@ -25,9 +25,34 @@ func connect(root *TreeNode) *TreeNode {
 	return root
 }
 
-type TreeNode struct {
+func connect1(root *Node) *Node {
+	start := root
+	for start != nil {
+		var nextStart, pre *Node
+		handle := func(cur *Node) {
+			if cur == nil {
+				return
+			}
+			if nextStart == nil {
+				nextStart = cur
+			}
+			if pre != nil {
+				pre.Next = cur
+			}
+			pre = cur
+		}
+		for p := start; p != nil; p = p.Next {
+			handle(p.Left)
+			handle(p.Right)
+		}
+		start = nextStart
+	}
+	return root
+}
+
+type Node struct {
 	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-	Next  *TreeNode
+	Left  *Node
+	Right *Node
+	Next  *Node
 }
