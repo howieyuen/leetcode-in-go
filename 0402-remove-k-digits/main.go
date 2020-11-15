b899aa877754071053562385546d850a05accc89
Hao <haoyuan@mail.ustc.edu.cn>
2020-11-15 22:21:24 +0800

    2020.11.15 leetcode打卡

---
+++ 0402-remove-k-digits/main.go
@@ -0,0 +1,22 @@
+package _402_remove_k_digits
+
+import (
+	"strings"
+)
+
+func removeKdigits(num string, k int) string {
+	var stack []byte
+	for i := range num {
+		for k > 0 && len(stack) > 0 && num[i] < stack[len(stack)-1] {
+			stack = stack[:len(stack)-1]
+			k--
+		}
+		stack = append(stack, num[i])
+	}
+	stack = stack[:len(stack)-k]
+	ans := strings.TrimLeft(string(stack), "0")
+	if ans == "" {
+		return "0"
+	}
+	return ans
+}
---
+++ 0402-remove-k-digits/main_test.go
@@ -0,0 +1,26 @@
+package _402_remove_k_digits
+
+import (
+	"testing"
+)
+
+func Test_removeKdigits(t *testing.T) {
+	type args struct {
+		num string
+		k   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{args: args{num: "112", k: 1}, want: "11"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeKdigits(tt.args.num, tt.args.k); got != tt.want {
+				t.Errorf("removeKdigits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
