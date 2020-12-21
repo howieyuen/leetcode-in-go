c5e44d8f72cbe68a0b7dbabedca513ca2fe9203b
howieyuen <haoyuan@mail.ustc.edu.cn>
2020-12-21 21:22:47 +0800

    467. 环绕字符串中唯一的子字符串

--- 0467-unique-substrings-in-wraparound-string/main.go
+++ 0467-unique-substrings-in-wraparound-string/main.go
@@ -1,1 +1,26 @@
 package _467_unique_substrings_in_wraparound_string
+
+func findSubstringInWraproundString(p string) int {
+	var dp = make([]int, 26)
+	var cur int
+	for i := 0; i < len(p); i++ {
+		if i > 0 && (p[i]-p[i-1] == 1 || p[i-1]-p[i] == 25) {
+			cur++
+		} else {
+			cur = 1
+		}
+		dp[p[i]-'a'] = max(dp[p[i]-'a'], cur)
+	}
+	var res int
+	for _, v := range dp {
+		res += v
+	}
+	return res
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
--- 0467-unique-substrings-in-wraparound-string/main_test.go
+++ 0467-unique-substrings-in-wraparound-string/main_test.go
@@ -1,1 +1,25 @@
 package _467_unique_substrings_in_wraparound_string
+
+import "testing"
+
+func Test_findSubstringInWraproundString(t *testing.T) {
+	type args struct {
+		p string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{args: args{p: "a"}, want: 1},
+		{args: args{p: "cac"}, want: 2},
+		{args: args{p: "zab"}, want: 6},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findSubstringInWraproundString(tt.args.p); got != tt.want {
+				t.Errorf("findSubstringInWraproundString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
