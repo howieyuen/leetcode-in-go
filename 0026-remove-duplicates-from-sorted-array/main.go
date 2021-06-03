fe78d4c093c76462e1af940b6df498db4d644450
howieyuen <haoyuan@mail.ustc.edu.cn>
2021-06-04 01:18:35 +0800

    bugfix: index out of range

--- 0026-remove-duplicates-from-sorted-array/main.go
+++ 0026-remove-duplicates-from-sorted-array/main.go
@@ -1,6 +1,9 @@
 package _026_remove_duplicates_from_sorted_array
 
 func removeDuplicates(nums []int) int {
+	if len(nums) <= 1 {
+		return len(nums)
+	}
 	var i = 0
 	for j := 1; j < len(nums); j++ {
 		for j < len(nums)-1 && nums[i] == nums[j] {
