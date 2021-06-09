0507c73d57d2cb6a8bc1fae0a81a0d53e903a2cb
howieyuen <haoyuan@mail.ustc.edu.cn>
2021-06-10 01:17:47 +0800

    add test func for 334

--- 0334-increasing-triplet-subsequence/main.go
+++ 0334-increasing-triplet-subsequence/main.go
@@ -19,3 +19,18 @@ func increasingTriplet(nums []int) bool {
 	}
 	return false
 }
+
+func increasingTriplet1(nums []int) bool {
+	var max = math.MaxInt32
+	var min = math.MaxInt32
+	for _, v := range nums{
+		if v > max { // 比最大值还大，满足
+			return true
+		} else if v > min { // 介于最大值和最小值之间，更新最大值，
+			max = v
+		} else { // 比最小值还小，更新最小值
+			min = v
+		}
+	}
+	return false
+}
