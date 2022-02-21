4714637b37d3b992621548681477798fbf0b2e6d
howieyuen <haoyuan@mail.ustc.edu.cn>
2022-02-21 16:01:20 +0800

    update 0014 longestCommonPrefix

--- 0014-longest-common-prefix/main.go
+++ 0014-longest-common-prefix/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	`fmt`
+	"strings"
 )
 
 /*
@@ -24,21 +24,14 @@ Explanation: There is no common prefix among the input strings.
 */
 
 func longestCommonPrefix(strs []string) string {
-	if len(strs) == 0 {return ""}
-	if len(strs) == 1 {return strs[0]}
-	mx := 0
-	for {
-		for i := 1; i < len(strs); i++ {
-			if mx >= len(strs[i-1]) || mx >= len(strs[i]) ||
-				strs[i-1][mx] != strs[i][mx] {
-				return strs[0][:mx]
-			}
-		}
-		mx++
+	if len(strs) == 0 {
+		return ""
 	}
-}
-
-func main() {
-	var strs = []string{"flower", "flow", "flight"}
-	fmt.Println(longestCommonPrefix(strs))
+	var res = strs[0]
+	for i := 1; i < len(strs); i++ {
+		for strings.Index(strs[i], res) != 0 {
+			res = res[0 : len(res)-1]
+		}
+	}
+	return res
 }
